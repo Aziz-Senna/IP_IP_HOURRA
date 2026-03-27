e673268cf2fafb759866a2d69fedcf3d1772bffe
abz_759 <amine.dede2024@gmail.com>
2026-03-27 08:30:14 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
-import pandas as pd
+<<<<<<< HEAD
+import Login as login
 
-def create_grid():
-    print("Notation CIDR               Binaire                           Décimal    Masque", end="\n")
-    datas = pd.read_csv('tableau_ip.csv', sep=';', encoding='latin-1')       
-    #Appeler le fichier csv qui contient les données du tableau, latin-1 pour accepter les accents et skiprows pour ignorer la première ligne du fichier csv
-    
-    datas = datas[["Notation CIDR", "Binaire", "Décimal", "Masque"]]    
-    #Sélectionner les colonnes qui m'interessent dans le fichier csv
-    
-    datas = datas.to_string(index=False, header=False)                                
-    #Ne pas afficher le numéro des lignes et ne pas afficher les colonnes    
-    print(datas)
-
-create_grid()
+#login.Connexion.verifier_bd("base_de_donnees.dat")
+login.Connexion.creer_administrateur("base_de_donnees.dat")
+=======
+>>>>>>>origin/main
